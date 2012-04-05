b55784ec82b6bb58922b9ed46938f86f452a6734
Oved Ourfali <oourfali@redhat.com>
2012-04-05 17:50:06 +0300

    reports: adding EngineAuthentication jar to support webadmin-reports integration
    This commit adds the Engine pre-authentication filter that supports
    identifying that a user is logged in to the engine, using its session
    ID, and supporting using the same user (which is external to Jasper) as
    a reports user, viewing reports.
    
    Notes:
    1. In order to use it, the EngineAuthentication.jar file should be put
    in "$JASPERSERVER_DEPLOYMENT_DIR"/WEB-INF/lib directory.
    2. Change
    "$JASPERSERVER_DEPLOYMENT_DIR"/WEB-INF/applicationContext-security-web.xml
    file:
    
    * Adding the EngineSimplePreAuthFilter filter to the filter chain for /**:
    
    /**=httpSessionContextIntegrationFilter,multipartRequestWrapperFilter,webAppSecurityFilter,jsCsrfGuardFilter,${bean.loggingFilter},${bean.userPreferencesFilter},${bean.authenticationProcessingFilter},${bean.userPreferencesFilter},${bean.basicProcessingFilter},EngineSimplePreAuthFilter,requestParameterAuthenticationFilter,JIAuthenticationSynchronizer,anonymousProcessingFilter,exceptionTranslationFilter,filterInvocationInterceptor,switchUserProcessingFilter,iPadSupportFilter
    
    * Add the following bean definition:
        <bean id="EngineSimplePreAuthFilter" class="org.ovirt.authentication.EngineSimplePreAuthFilter">
            <property name="authenticationManager">
                <ref bean="authenticationManager"/>
            </property>
            <property name="servletURL" value="http://127.0.0.1:8080/OvirtEngineWeb/ValidateSession"/>
            <property name="pollingTimeout" value="10"/>
            <property name="trustStorePath" value="<path to trust store>"/>
            <property name="trustStorePassword" value="<password to trust store>"/>
        </bean>
    
    The SSL-related properties have to be added only in case the servlet URL
    starts with https (secured connection).
    
    3. Currently there is a problem with the jasperserver maven repo, so the
    fetched jar is corrupted. Waiting for their resolution.
    
    Change-Id: I14b3803ef899434dade1e6189b9c5c26a41dd190

---
+++ reports/jars/EngineAuthentication/src/main/java/org/ovirt/authentication/EngineSimplePreAuthFilter.java
@@ -0,0 +1,260 @@
+package org.ovirt.authentication;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.GrantedAuthorityImpl;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
+import org.springframework.security.ui.AuthenticationDetailsSource;
+import org.springframework.security.ui.FilterChainOrder;
+import org.springframework.security.ui.WebAuthenticationDetailsSource;
+import org.springframework.security.ui.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import com.jaspersoft.jasperserver.api.metadata.user.domain.impl.client.MetadataUserDetails;
+
+/*
+ * This class is a pre-authentication filter for the oVirt engine.
+ * The purpose is to support using the Reports server from within the oVirt web admin.
+ * It gets a session ID, and validates it using the oVirt engine, getting the logged-in user.
+ */
+public class EngineSimplePreAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+    protected AuthenticationDetailsSource authenticationDetailsSource = new WebAuthenticationDetailsSource();
+    // Will be set using the bean properties defined in applicationContext-security-web.xml file
+    private String servletURL;
+    private int pollingTimeout;
+    private String SESSION_DATA_FORMAT = "sessionID=%1$s";
+    private int DEFAULT_SESSION_TIMEOUT = 20;
+    private String trustStorePath;
+    private String trustStorePassword;
+    private String sslProtocol = "TLS";
+    private String trustStoreType = "JKS";
+    private final Log logger = LogFactory.getLog(EngineSimplePreAuthFilter.class);
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest arg0) {
+        return null;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest arg0) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return FilterChainOrder.PRE_AUTH_FILTER;
+    }
+
+    @Override
+    public void doFilterHttp(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            SecurityContextHolder.getContext().setAuthentication(getAuthRequest(request));
+        } else {
+            // The logic here is that if we are already authenticated, and the authentication was done in this pre-auth filter, then we check if we need
+            // to re-check our token
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof MetadataUserDetails) {
+                MetadataUserDetails metadataUserDetails = (MetadataUserDetails) principal;
+                UsernamePasswordAuthenticationToken originalAuthentication = (UsernamePasswordAuthenticationToken) metadataUserDetails.getOriginalAuthentication();
+
+                if (originalAuthentication != null && originalAuthentication.getPrincipal() instanceof EngineUserDetails) {
+                    EngineUserDetails userDetails = (EngineUserDetails) originalAuthentication.getPrincipal();
+                    // Checking if we need to re-check the session, and acting accordingly
+                    if (userDetails.isRecheckSessionIdNeeded()) {
+                        SecurityContextHolder.getContext().setAuthentication(getAuthRequest(request, userDetails.getUserSessionID()));
+                    }
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    /*
+     * This method creates the URL connection, whether it is a secured connection or not.
+     */
+    private HttpURLConnection createURLConnection() throws IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException, KeyManagementException {
+        boolean secured = servletURL.startsWith("https");
+
+        URL url = new URL(servletURL);
+        HttpURLConnection servletConnection;
+
+        if (secured) {
+            if (trustStorePassword == null || trustStorePath == null) {
+                logger.error("The Supplied URL is secured, however no trust store path or password were supplied.");
+                return null;
+            }
+            HttpsURLConnection securedConnection = (HttpsURLConnection) url.openConnection();
+            KeyStore trustStore = KeyStore.getInstance(trustStoreType);
+            trustStore.load(new FileInputStream(trustStorePath), trustStorePassword.toCharArray());
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(trustStore);
+            SSLContext ctx = SSLContext.getInstance(sslProtocol);
+            ctx.init(null, trustManagerFactory.getTrustManagers(), null);
+            securedConnection.setSSLSocketFactory(ctx.getSocketFactory());
+            servletConnection = securedConnection;
+        } else {
+            servletConnection = (HttpURLConnection) url.openConnection();
+        }
+
+        servletConnection.setRequestMethod("POST");
+        servletConnection.setDoOutput(true);
+        servletConnection.setDoInput(true);
+        servletConnection.setReadTimeout(10000);
+        servletConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+
+        return servletConnection;
+    }
+
+    /*
+     * This method gets a sessionID, and validates it with the engine, using the servlet input URL
+     */
+    protected String callValidateSession(String sessionID) {
+        DataOutputStream output = null;
+
+        try {
+            // Formatting data
+            String data = String.format(SESSION_DATA_FORMAT, URLEncoder.encode(sessionID, "UTF-8"));
+
+            HttpURLConnection servletConnection = createURLConnection();
+
+            if (servletConnection == null) {
+                logger.error("Unable to create servlet connection.");
+                return null;
+            }
+
+            // Sending the sessionID parameter
+            output = new DataOutputStream(servletConnection.getOutputStream());
+            output.writeBytes(data);
+            output.flush();
+            output.close();
+
+            // Checking the result
+            if (servletConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                logger.error("ValidateSession servlet returned " + servletConnection.getResponseCode());
+                return null;
+            }
+
+            // Getting the user name
+            InputStream inputStream = servletConnection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String inputLine;
+            inputLine = reader.readLine();
+            return inputLine;
+
+        } catch (Exception ex) {
+            logger.error(ex);
+        } finally {
+            try {
+                if (output != null)
+                output.close();
+            } catch (IOException ex) {
+                logger.error(ex);
+            }
+        }
+        return null;
+    }
+
+    public UsernamePasswordAuthenticationToken getAuthRequest(HttpServletRequest request) {
+        String sessionID = request.getParameter("sessionID");
+        return getAuthRequest(request, sessionID);
+    }
+
+    /*
+     * This method gets the request, and the sesionID, and resulting in the UsernamePasswordAuthenticationToken of the engine-authenticated user
+     * that's logged in to the engine.
+     */
+    public UsernamePasswordAuthenticationToken getAuthRequest(HttpServletRequest request, String sessionID) {
+        UsernamePasswordAuthenticationToken authRequest = null;
+        if (sessionID != null) {
+            String result = callValidateSession(sessionID);
+            if (result == null) {
+                return null;
+            } else if (result.isEmpty()) {
+                return null;
+            }
+
+            String userName = result;
+            String password = "";
+
+            userName = userName.trim();
+            GrantedAuthority[] grantedAuthorities = new GrantedAuthority[1];
+            grantedAuthorities[0] = new GrantedAuthorityImpl("ROLE_USER");
+
+            Calendar recheckOn = Calendar.getInstance();
+            recheckOn.setTime(new Date());
+            recheckOn.add(Calendar.MINUTE, pollingTimeout);
+
+            EngineUserDetails userDetails = new EngineUserDetails(userName, password, grantedAuthorities, sessionID, recheckOn, true, true, true, true);
+            authRequest = new UsernamePasswordAuthenticationToken(userDetails, password, grantedAuthorities);
+            setDetails(request, authRequest);
+        }
+
+        return authRequest;
+    }
+
+    protected void setDetails(HttpServletRequest request, UsernamePasswordAuthenticationToken authRequest) {
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+    }
+
+    public String getServletURL() {
+        return servletURL;
+    }
+
+    public void setServletURL(String servletURL) {
+        this.servletURL = servletURL;
+    }
+
+    public int getPollingTimeout() {
+        return pollingTimeout;
+    }
+
+    public void setPollingTimeout(int pollingTimeout) {
+        if (pollingTimeout >= 0) {
+            this.pollingTimeout = pollingTimeout;
+        } else {
+            logger.debug("Input session timeout was a negative value. Setting it to the default timeout, " + DEFAULT_SESSION_TIMEOUT);
+            this.pollingTimeout = DEFAULT_SESSION_TIMEOUT;
+        }
+    }
+
+    public void setTrustStorePath(String trustStorePath) {
+        this.trustStorePath = trustStorePath;
+    }
+
+    public void setTrustStorePassword(String trustStorePassword) {
+        this.trustStorePassword = trustStorePassword;
+    }
+
+    public void setSslProtocol(String sslProtocol) {
+        this.sslProtocol = sslProtocol;
+    }
+}
---
+++ reports/jars/EngineAuthentication/src/main/java/org/ovirt/authentication/EngineUserDetails.java
@@ -0,0 +1,93 @@
+package org.ovirt.authentication;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
+
+public class EngineUserDetails implements UserDetails {
+
+    public EngineUserDetails(String userName,
+            String password,
+            GrantedAuthority[] authorities,
+            String userSessionID,
+            Calendar recheckSessionIdOn,
+            boolean isAccountNonExpired,
+            boolean isAccountNonLocked,
+            boolean isCredentialsNonExpired,
+            boolean isEnabled) {
+        super();
+        this.userName = userName;
+        this.password = password;
+        this.authorities = authorities;
+        this.userSessionID = userSessionID;
+        this.recheckSessionIdOn = recheckSessionIdOn;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    private static final long serialVersionUID = -7928501868572473559L;
+
+    private String userName;
+    private String password;
+    private GrantedAuthority[] authorities;
+    private String userSessionID;
+    private Calendar recheckSessionIdOn;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+
+    @Override
+    public GrantedAuthority[] getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public boolean isRecheckSessionIdNeeded() {
+        Calendar currDate = Calendar.getInstance();
+        currDate.setTime(new Date());
+        return currDate.after(recheckSessionIdOn);
+    }
+
+    public String getUserSessionID() {
+        return userSessionID;
+    }
+
+    public void setUserSessionID(String userSessionID) {
+        this.userSessionID = userSessionID;
+    }
+
+}
