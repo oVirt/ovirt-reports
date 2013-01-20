8c9b5f40a6380e487575567ba5c5d36bbc3e5f87
Yaniv Dary <ydary@redhat.com>
2013-01-20 12:39:34 +0200

    reports: added ic variable to allow date localzation (#892306)
    Bug-Url: https://bugzilla.redhat.com/892306
    
    Change-Id: Ifb897603a565be3dff853340069820931f82e3ef
    Signed-off-by: Yaniv Dary <ydary@redhat.com>

--- reports/jars/CustomOvirtReportsQueryManipulator/src/main/java/org/ovirt/jasperreports/querymodifier/CustomOvirtReportsQueryManipulator.java
+++ reports/jars/CustomOvirtReportsQueryManipulator/src/main/java/org/ovirt/jasperreports/querymodifier/CustomOvirtReportsQueryManipulator.java
@@ -1,12 +1,18 @@
 package org.ovirt.jasperreports.querymodifier;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.jaspersoft.jasperserver.api.engine.common.service.IQueryManipulator;
 import com.jaspersoft.jasperserver.war.common.JasperServerUtil;
 
 public class CustomOvirtReportsQueryManipulator implements IQueryManipulator {
     public final static String USERLOCALE = "$P{userlocale}";
+    public final static String DATETIMELOCALIZEDPATTERN = "$P{datetimelocalepattern}";
+    public final static String DATELOCALIZEDPATTERN = "$P{datelocalepattern}";
 
     public IQueryManipulator cascading;
 
@@ -16,8 +22,54 @@ public class CustomOvirtReportsQueryManipulator implements IQueryManipulator {
         String cascadingQuery = cascading.updateQuery(query, parameters);
         Locale locale = JasperServerUtil.getExecutionContext().getLocale();
         String userLocale = locale.toString();
+        String dateTimeLocalizedpatteren = getPostgresDatePatternString(locale, true);
+        String dateLocalizedpatteren = getPostgresDatePatternString(locale, false);
+
         cascadingQuery = cascadingQuery.replace(USERLOCALE, "'" + userLocale + "'");
+        cascadingQuery = cascadingQuery.replace(DATETIMELOCALIZEDPATTERN, "'" + dateTimeLocalizedpatteren + "'");
+        cascadingQuery = cascadingQuery.replace(DATELOCALIZEDPATTERN, "'" + dateLocalizedpatteren + "'");
         return cascadingQuery;
+    }
+
+    public String getPostgresDatePatternString(Locale locale, Boolean time) {
+        SimpleDateFormat dateLocalizedPattern;
+        if (time) {
+            dateLocalizedPattern = (SimpleDateFormat)SimpleDateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, locale);
+        }
+        else {
+            dateLocalizedPattern = (SimpleDateFormat)SimpleDateFormat.getDateInstance(DateFormat.SHORT, locale);
+        }
+        String dateLocalizedPatternString = dateLocalizedPattern.toPattern();
+        /*year changes*/
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("y", "Y");
+        /*day changes*/
+        if (StringUtils.countMatches(dateLocalizedPatternString, "M") == 1) {
+            dateLocalizedPatternString = dateLocalizedPatternString.replace("M", "MM");
+        }
+        /*day changes*/
+        if (StringUtils.countMatches(dateLocalizedPatternString, "d") == 1) {
+            dateLocalizedPatternString = dateLocalizedPatternString.replace("d", "dd");
+        }
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("dd", "DD");
+        /*hour changes*/
+        if (StringUtils.countMatches(dateLocalizedPatternString, "H") == 1) {
+            dateLocalizedPatternString = dateLocalizedPatternString.replace("H", "HH");
+        }
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("HH", "HH24");
+        if (StringUtils.countMatches(dateLocalizedPatternString, "h") == 1) {
+            dateLocalizedPatternString = dateLocalizedPatternString.replace("h", "hh");
+        }
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("hh", "HH12");
+        /*minute changes*/
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("mm", "MI");
+        /*second changes*/
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("ss", "SS");
+        /*am\pm changes*/
+        if (StringUtils.countMatches(dateLocalizedPatternString, "a") == 1) {
+            dateLocalizedPatternString = dateLocalizedPatternString.replace("a", "aa");
+        }
+        dateLocalizedPatternString = dateLocalizedPatternString.replace("aa", "AM");
+        return dateLocalizedPatternString;
     }
 
     public IQueryManipulator getCascading() {
