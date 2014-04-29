8cf433e829e8b919d49a520aaf1f73d3bd50519f
Yedidyah Bar David <didi@redhat.com>
2014-04-29 12:07:13 +0300

    packaging: setup: Ask only during setup
    Move customization question 'Configure Reports on this host' from common
    to setup so that it's not asked during cleanup (or rename).
    
    Change-Id: I2360968a46a660c0634a25ef1d91fb1f2da92ce8
    Bug-Url: https://bugzilla.redhat.com/1080431
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-reports/core/misc.py
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-reports/core/misc.py
@@ -1,6 +1,6 @@
 #
 # ovirt-engine-setup -- ovirt engine setup
-# Copyright (C) 2013 Red Hat, Inc.
+# Copyright (C) 2014 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ from otopi import plugin
 
 from ovirt_engine_setup import constants as osetupcons
 from ovirt_engine_setup import reportsconstants as oreportscons
+from ovirt_engine_setup import dialog
 
 
 @util.export
@@ -35,31 +36,29 @@ class Plugin(plugin.PluginBase):
         super(Plugin, self).__init__(context=context)
 
     @plugin.event(
-        stage=plugin.Stages.STAGE_INIT,
+        stage=plugin.Stages.STAGE_CUSTOMIZATION,
+        name=oreportscons.Stages.CORE_ENABLE,
+        before=(
+            osetupcons.Stages.DIALOG_TITLES_E_PRODUCT_OPTIONS,
+            oreportscons.Stages.DB_CONNECTION_CUSTOMIZATION,
+        ),
+        after=(
+            osetupcons.Stages.DIALOG_TITLES_S_PRODUCT_OPTIONS,
+        ),
     )
-    def _init(self):
-        self.environment.setdefault(oreportscons.CoreEnv.ENABLE, None)
-
-    @plugin.event(
-        stage=plugin.Stages.STAGE_SETUP,
-    )
-    def _setup(self):
-        self.environment[
-            osetupcons.CoreEnv.REGISTER_UNINSTALL_GROUPS
-        ].createGroup(
-            group='ovirt_reports_files',
-            description='Reports files',
-            optional=True,
-        )
-        self.environment[
-            osetupcons.CoreEnv.SETUP_ATTRS_MODULES
-        ].append(oreportscons)
-        self.logger.debug(
-            'reports version: %s-%s (%s)\n',
-            oreportscons.Const.PACKAGE_NAME,
-            oreportscons.Const.PACKAGE_VERSION,
-            oreportscons.Const.DISPLAY_VERSION,
-        )
-
+    def _customization(self):
+        if self.environment[oreportscons.CoreEnv.ENABLE] is None:
+            self.environment[
+                oreportscons.CoreEnv.ENABLE
+            ] = dialog.queryBoolean(
+                dialog=self.dialog,
+                name='OVESETUP_REPORTS_ENABLE',
+                note=_(
+                    'Configure Reports on this host '
+                    '(@VALUES@) [@DEFAULT@]: '
+                ),
+                prompt=True,
+                default=True,
+            )
 
 # vim: expandtab tabstop=4 shiftwidth=4
